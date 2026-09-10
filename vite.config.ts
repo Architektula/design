41eb88b12261e4dfa12f0375c3caf559330d37b7
Architektula <n-concept@mail.ru>
2026-09-10 16:07:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-const basePath = "/design/";
+const basePath = "/";
 export default defineConfig({
   base: basePath,
 
